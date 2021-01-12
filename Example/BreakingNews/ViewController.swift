7cd6628a38e76baffe06ad3ca230bb2358fb7b38
Egor A. Bedunkevich <goshabedunkevich@gmail.com>
2021-01-13 00:02:26 +0100

    Example

--- Example/BreakingNews/ViewController.swift
+++ Example/BreakingNews/ViewController.swift
@@ -7,18 +7,31 @@
 //
 
 import UIKit
+import BreakingNews
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, BreakingDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Make sure to embed the view in a navigation controller. The view must also be a subclass of Breaking
+        
+        /*
+         Networking.......
+         */
+        
+        // The view will be dismissed automatically when the user taps on it.
+        let breaking = self.navigationController as! Breaking
+        breaking.news(nil, .link, .white)
+        breaking.breakingNewsDelegate = self
+        
     }
-
+    
+    func didPressAlert() {
+        
+        /*
+         Do something extra.....
+         */
+    }
 }
 
